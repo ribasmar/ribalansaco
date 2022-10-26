b8a59e1459a3c858d2e437c8efddd5dd938cccb4
ribasmar <ribas7494@gmail.com>
2022-10-26 20:56:36 -0300

    Update trabalho.c

--- trabalho.c
+++ trabalho.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
+typedef struct
+{
+
+  char codigo[6];
+  char titulo[20];
+  char secao[5];
+  char tipo[10];
+
+}Figurinha;//struct Figurinha
+
+typedef struct
+{
+    int coladas;
+    int faltando;
+    float valorgasto;
+    char dono[20];
+
+}Album;//struct Album
+
+
+
+int main(){
+
+
+  return 0;
+}//main
